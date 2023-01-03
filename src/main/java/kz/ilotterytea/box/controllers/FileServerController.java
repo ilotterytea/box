7b08e0ed985c8eb03812f445d27e9b0e1d8a6fe0
ilotterytea <ilotterytea@proton.me>
2023-01-04 03:22:30 +0600

    Return 201 instead of 200

--- src/main/java/kz/ilotterytea/box/controllers/FileServerController.java
+++ src/main/java/kz/ilotterytea/box/controllers/FileServerController.java
@@ -9,6 +9,7 @@ import kz.ilotterytea.box.utils.KeyUtils;
 import org.apache.tika.mime.MimeTypeException;
 import org.apache.tika.mime.MimeTypes;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -146,6 +147,6 @@ public class FileServerController {
             models[i] = processFile(files[i]);
         }
 
-        return ResponseEntity.ok(models);
+        return ResponseEntity.status(HttpStatus.CREATED).body(models);
    }
 }
