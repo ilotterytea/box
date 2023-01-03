a2af2a29956a42e5afd0b7e71f01e3df1d129719
ilotterytea <ilotterytea@proton.me>
2023-01-03 08:39:11 +0600

    A controller for file serving

---
+++ src/main/java/kz/ilotterytea/box/controllers/FileServerController.java
@@ -0,0 +1,72 @@
+package kz.ilotterytea.box.controllers;
+
+import kz.ilotterytea.box.BoxProperties;
+import kz.ilotterytea.box.models.FileDataModel;
+import kz.ilotterytea.box.utils.KeyUtils;
+import org.apache.tika.mime.MimeTypeException;
+import org.apache.tika.mime.MimeTypes;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * A controller for file serving: uploading, sharing, deleting.
+ * @author ilotterytea
+ * @since 1.0
+ */
+@Controller
+public class FileServerController {
+    private BoxProperties properties;
+
+    @Autowired
+    public FileServerController(BoxProperties properties) {
+        this.properties = properties;
+    }
+
+    private FileDataModel processFile(MultipartFile file) {
+        FileDataModel model = null;
+
+        try {
+            model = new FileDataModel(
+                    KeyUtils.generateCharKey(properties.getChars(), properties.getDefaultIdLength()),
+                    file.getContentType(),
+                    MimeTypes.getDefaultMimeTypes().forName(file.getContentType()).getExtension()
+            );
+        } catch (MimeTypeException e) {
+            e.printStackTrace();
+        }
+
+        if (!new File(properties.getUploadedPath()).exists()) {
+            new File(properties.getUploadedPath()).mkdirs();
+        }
+
+        try {
+            file.transferTo(new File(String.format(
+                    "%s/%s%s",
+                    properties.getUploadedPath(),
+                    (model != null) ? model.getId() : file.getOriginalFilename(),
+                    (model != null) ? model.getExt() : ""
+            )));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return model;
+    }
+
+    @PostMapping("/upload")
+    public ResponseEntity<FileDataModel[]> handleFileUpload(@RequestParam("files") MultipartFile[] files) {
+        FileDataModel[] models = new FileDataModel[files.length];
+        for (int i = 0; i < files.length; i++) {
+            models[i] = processFile(files[i]);
+        }
+
+        return ResponseEntity.ok(models);
+   }
+}
