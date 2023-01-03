9950b64b2932f660f53f301b4e812deb58cd8aeb
ilotterytea <ilotterytea@proton.me>
2023-01-03 08:11:50 +0600

    default ID length

--- src/main/java/kz/ilotterytea/box/BoxProperties.java
+++ src/main/java/kz/ilotterytea/box/BoxProperties.java
@@ -13,7 +13,12 @@ public class BoxProperties {
     /** Character pool. */
     @Value("${box.chars}")
     private String chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_";
+    @Value("${box.defaultIdLength}")
+    private int defaultIdLength = 6;
 
     public void setChars(String chars) { this.chars = chars; }
     public String getChars() { return chars; }
+
+    public void setDefaultIdLength(int defaultIdLength) { this.defaultIdLength = defaultIdLength; }
+    public int getDefaultIdLength() { return defaultIdLength; }
 }
