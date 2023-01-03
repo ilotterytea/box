febe15a3ec57c5e7e7ae864b8fb59dbb1371cb6a
ilotterytea <ilotterytea@proton.me>
2023-01-04 04:12:22 +0600

    Updated the Thymeleaf controller.

--- src/main/java/kz/ilotterytea/box/controllers/ThymeleafController.java
+++ src/main/java/kz/ilotterytea/box/controllers/ThymeleafController.java
@@ -1,8 +1,12 @@
 package kz.ilotterytea.box.controllers;
 
+import kz.ilotterytea.box.BoxProperties;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * A controller for Thymeleaf web templates.
@@ -11,9 +15,17 @@ import org.springframework.web.bind.annotation.GetMapping;
  */
 @Controller
 public class ThymeleafController {
+    private BoxProperties properties;
+
+    @Autowired
+    public ThymeleafController(BoxProperties properties) {
+        this.properties = properties;
+    }
+
     @GetMapping("/")
-    public String serveIndex(Model model) {
-        model.addAttribute("test", "test");
+    public String serveIndex(Model model, HttpServletRequest request) {
+        model.addAttribute("title", properties.getBoxName());
+        model.addAttribute("web_url", request.getRequestURL().toString() + "web");
 
         return "index";
     }
