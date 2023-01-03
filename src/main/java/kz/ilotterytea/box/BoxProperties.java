b28db8b90da10b9d54f69b3202d5b362fe9b9ce7
ilotterytea <ilotterytea@proton.me>
2023-01-03 08:16:50 +0600

    Path to the folder where the uploaded files are to be stored.

--- src/main/java/kz/ilotterytea/box/BoxProperties.java
+++ src/main/java/kz/ilotterytea/box/BoxProperties.java
@@ -13,12 +13,19 @@ public class BoxProperties {
     /** Character pool. */
     @Value("${box.chars}")
     private String chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_";
+    /** Default length for ID. */
     @Value("${box.defaultIdLength}")
     private int defaultIdLength = 6;
+    /** Path to the folder where the uploaded files are to be stored. */
+    @Value("${box.uploadedPath}")
+    private String uploadedPath = "./uploaded";
 
     public void setChars(String chars) { this.chars = chars; }
     public String getChars() { return chars; }
 
     public void setDefaultIdLength(int defaultIdLength) { this.defaultIdLength = defaultIdLength; }
     public int getDefaultIdLength() { return defaultIdLength; }
+
+    public void setUploadedPath(String uploadedPath) { this.uploadedPath = uploadedPath; }
+    public String getUploadedPath() { return uploadedPath; }
 }
