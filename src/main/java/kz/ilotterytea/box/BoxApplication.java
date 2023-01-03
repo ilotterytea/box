83878c8c1c09c72c9e6a6ae2115ed2454e08f531
ilotterytea <ilotterytea@proton.me>
2023-01-03 08:40:14 +0600

    Enable configuration properties.

--- src/main/java/kz/ilotterytea/box/BoxApplication.java
+++ src/main/java/kz/ilotterytea/box/BoxApplication.java
@@ -2,8 +2,10 @@ package kz.ilotterytea.box;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(BoxProperties.class)
 public class BoxApplication {
 
 	public static void main(String[] args) {
