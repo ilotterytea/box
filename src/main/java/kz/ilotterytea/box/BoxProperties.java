83ae30cc75bc1856c0df06a1cb75abb8b61b62ed
ilotterytea <ilotterytea@proton.me>
2023-01-04 01:01:45 +0600

    fix: The app will not start if you don't fill in the fields in application.properties

--- src/main/java/kz/ilotterytea/box/BoxProperties.java
+++ src/main/java/kz/ilotterytea/box/BoxProperties.java
@@ -1,6 +1,5 @@
 package kz.ilotterytea.box;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 /**
@@ -8,16 +7,13 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
  * @author ilotterytea
  * @since 1.0
  */
-@ConfigurationProperties("box")
+@ConfigurationProperties(prefix = "box")
 public class BoxProperties {
     /** Character pool. */
-    @Value("${box.chars}")
     private String chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_";
     /** Default length for ID. */
-    @Value("${box.defaultIdLength}")
     private int defaultIdLength = 6;
     /** Path to the folder where the uploaded files are to be stored. */
-    @Value("${box.uploadedPath}")
     private String uploadedPath = "./uploaded";
 
     public void setChars(String chars) { this.chars = chars; }
