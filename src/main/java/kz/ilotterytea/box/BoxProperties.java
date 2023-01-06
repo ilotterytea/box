3ec952209f74514ae9538b733ad2bcb4b9448a5d
ilotterytea <ilotterytea@proton.me>
2023-01-07 01:20:01 +0600

    Unnecessary setters

--- src/main/java/kz/ilotterytea/box/BoxProperties.java
+++ src/main/java/kz/ilotterytea/box/BoxProperties.java
@@ -36,33 +36,14 @@ public class BoxProperties {
     /** Generate a hash summary of uploaded file. */
     private boolean generateHashSum = true;
 
-    public void setChars(String chars) { this.chars = chars; }
     public String getChars() { return chars; }
-
-    public void setDefaultIdLength(int defaultIdLength) { this.defaultIdLength = defaultIdLength; }
     public int getDefaultIdLength() { return defaultIdLength; }
-
-    public void setUploadedPath(String uploadedPath) { this.uploadedPath = uploadedPath; }
     public String getUploadedPath() { return uploadedPath; }
-
-    public void setDataPath(String dataPath) { this.dataPath = dataPath; }
     public String getDataPath() { return dataPath; }
-
-    public void setBoxName(String boxName) { this.boxName = boxName; }
     public String getBoxName() { return boxName; }
-
-    public void setMaxFileAgeMs(long maxFileAgeMs) { this.maxFileAgeMs = maxFileAgeMs; }
     public long getMaxFileAgeMs() { return maxFileAgeMs; }
-
-    public void setCollectExpiredFilesIntervalMs(long collectExpiredFilesIntervalMs) { this.collectExpiredFilesIntervalMs = collectExpiredFilesIntervalMs; }
     public long getCollectExpiredFilesIntervalMs() { return collectExpiredFilesIntervalMs; }
-
-    public void setUseExpiredFilesCollector(boolean useExpiredFilesCollector) { this.useExpiredFilesCollector = useExpiredFilesCollector; }
     public boolean getUseExpiredFilesCollector() { return useExpiredFilesCollector; }
-
-    public void setGenerateDataFiles(boolean generateDataFiles) { this.generateDataFiles = generateDataFiles; }
     public boolean getGenerateDataFiles() { return generateDataFiles; }
-
-    public void setGenerateHashSum(boolean generateHashSum) { this.generateHashSum = generateHashSum; }
     public boolean getGenerateHashSum() { return generateHashSum; }
 }
