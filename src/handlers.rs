7ccd7d55a6c0332124e50c1d4ad1d4460d9d65ce
ilotterytea <ilotterytea@proton.me>
2023-02-18 17:47:22 +0600

    POST /upload endpoint

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,4 +1,24 @@
+use rocket::form::Form;
+use rocket::fs::TempFile;
+use rocket::http::Status;
+use rocket::response::status::Custom;
+use rocket::serde::json::Json;
+
+use crate::file::FileData;
+
 #[get("/")]
 pub fn index() -> &'static str {
     "hello, my hateful world!"
+}
+
+#[post("/upload", data = "<file>")]
+pub async fn upload(mut file: Form<TempFile<'_>>) -> Result<Json<FileData>, Custom<String>> {
+    let data = FileData::new(6, &file);
+
+    file.copy_to(format!("./uploaded/{}.{}", data.id, data.ext))
+        .await
+        .map_err(|e| Custom(Status::InternalServerError, e.to_string()))
+        .ok();
+
+    Ok(Json(data))
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 #[macro_use]
 extern crate rocket;
 
+mod file;
 mod handlers;
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![handlers::index])
+    rocket::build().mount("/", routes![handlers::index, handlers::upload])
 }
