6398448a4c924bb44eb217ea31b288856f5b1e89
ilotterytea <ilotterytea@proton.me>
2023-02-18 18:03:49 +0600

    Create folder for uploaded files.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,18 @@
 #[macro_use]
 extern crate rocket;
 
+use std::fs::create_dir_all;
+
 mod file;
 mod handlers;
 
 #[launch]
 fn rocket() -> _ {
+    create_dir_all("./uploaded").map_err(|e| {
+        panic!(
+            "Unable to create directory for uploaded files: {}",
+            e.to_string()
+        )
+    });
     rocket::build().mount("/", routes![handlers::index, handlers::upload])
 }
