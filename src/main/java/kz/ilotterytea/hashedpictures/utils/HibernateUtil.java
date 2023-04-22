cf56799d0b7f91f144e2617c97e95d73ac3a0678
ilotterytea <ilotterytea@proton.me>
2023-04-23 03:33:58 +0600

    add: hibernate util

---
+++ src/main/java/kz/ilotterytea/hashedpictures/utils/HibernateUtil.java
@@ -0,0 +1,22 @@
+package kz.ilotterytea.hashedpictures.utils;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+/**
+ * @author ilotterytea
+ * @since 1.0
+ */
+public class HibernateUtil {
+    private static final SessionFactory sessionFactory = generateSessionFactory();
+
+    private static SessionFactory generateSessionFactory() {
+        return new Configuration()
+                .configure()
+                .buildSessionFactory();
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+}
