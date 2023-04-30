d3e8e2e5015569417b02547e076af484ee965d56
ilotterytea <ilotterytea@proton.me>
2023-04-30 13:50:41 +0600

    add: application properties

---
+++ src/main/java/kz/ilotterytea/hashedpictures/ApplicationProperties.java
@@ -0,0 +1,26 @@
+package kz.ilotterytea.hashedpictures;
+
+import io.micronaut.context.annotation.ConfigurationProperties;
+
+/**
+ * @author ilotterytea
+ * @since 1.0
+ */
+@ConfigurationProperties("bin")
+public class ApplicationProperties {
+    private String tokenChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_";
+    private int tokenLength = 6;
+    private int keyLength = 16;
+
+    public String getTokenChars() {
+        return tokenChars;
+    }
+
+    public Integer getTokenLength() {
+        return tokenLength;
+    }
+
+    public int getKeyLength() {
+        return keyLength;
+    }
+}
